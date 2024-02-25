e09fb67a676768666644fbb93a80147a5ff0c8b9
sstresnak <sastresnak@gmail.com>
2024-02-25 14:34:45 -0600

    added jquery and styling

---
+++ js/index.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    let isPopupOpen = false; // Flag to track popup state
+
+    // Function to close all popups
+    function closeAllPopups() {
+        $('.popup').hide();
+        isPopupOpen = false; // Reset popup state
+    }
+
+    // Show popup for Skill 1 when clicked
+    $('#skill1').click(function(){
+        if (!isPopupOpen) {
+            closeAllPopups(); // Close all other popups
+            $('#popupSkill1').show();
+            isPopupOpen = true; // Set popup state
+        }
+    });
+
+    // Show popup for Skill 2 when clicked
+    $('#skill2').click(function(){
+        if (!isPopupOpen) {
+            closeAllPopups(); // Close all other popups
+            $('#popupSkill2').show();
+            isPopupOpen = true; // Set popup state
+        }
+    });
+
+    // Show popup for Skill 3 when clicked
+    $('#skill3').click(function(){
+        if (!isPopupOpen) {
+            closeAllPopups(); // Close all other popups
+            $('#popupSkill3').show();
+            isPopupOpen = true; // Set popup state
+        }
+    });
+
+    // Close button functionality
+    $('.closeButton').click(function(){
+        $(this).parent('.popup').hide();
+        isPopupOpen = false; // Reset popup state when closing
+    });
+});
